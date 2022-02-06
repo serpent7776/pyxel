cdb9091c0a68a5177828329f89e3d642f40a783c
Serpent7776 <serpent7776@gmail.com>
2022-02-06 17:14:08 +0100

    Add FreeBSD support

--- pyxel/__init__.py
+++ pyxel/__init__.py
@@ -8,5 +8,7 @@ elif _system == "Windows":
     from .lib.windows.pyxel_wrapper import *  # type: ignore  # noqa F403
 elif _system == "Linux":
     from .lib.linux.pyxel_wrapper import *  # type: ignore  # noqa F403
+elif _system == "FreeBSD":
+    from .lib.freebsd.pyxel_wrapper import *  # type: ignore  # noqa F403
 else:
     raise Exception("unsupported platform")
